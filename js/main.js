17f9035cee0a53de34073928ddb292cace5d18b7
Taras Borovets <legioner.ukr@gmail.com>
2016-12-05 15:22:10 +0000

    fff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-// JavaScript File
 $(document).ready(function () {
-    $('italic').fadeIn(4000).removeClass('hidden');
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
 });
